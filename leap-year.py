a374c553fd8fe5bd543463cec7c85eb9847e1533
abdullahkayhan38 <abdullahkayhan@hotmail.com>
2021-11-23 19:18:23 +0300

    python dosyasına ekleme yapıldı.

--- leap-year.py
+++ leap-year.py
@@ -1,3 +1,4 @@
+
 # To get year (integer input) from the user
 # This is my first explanation
 year = int(input("Enter a year: "))
@@ -12,3 +13,5 @@ if (year % 4) == 0:
         print("{0} is a leap year".format(year))
 else:
     print("{0} is not a leap year".format(year))
+    print("değişiklik yapıldı")
+
